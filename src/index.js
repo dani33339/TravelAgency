851dde4195de604a0317aa240ad7a46a3bde88f2
kiliman1995@gmail.com <kiliman1995@gmail.com>
2022-11-16 14:23:38 +0200

    initial stage

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+
+
+const root = ReactDOM.render( <App />, document.getElementById('root'))
+root.render(<App/>)
