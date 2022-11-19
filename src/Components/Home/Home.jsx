ce113907836a502325095e07aa975bca464794ea
kiliman1995@gmail.com <kiliman1995@gmail.com>
2022-11-19 19:03:44 +0200

    continue with the search

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './home.css'
 import video from '../../Assets/video3.mp4'
+import {GrLocation} from 'react-icons/gr'
 
 const Home = () => {
     return (
@@ -20,12 +21,33 @@ const Home = () => {
 
                 </div>
                 <div className="cardDiv grid">
+
                     <div className="destinationInput">
                         <label htmlFor="city">Search your destination:</label>
                         <div className="input flex">
                             <input type="text" placeholder="Enter name here..."/>
+                            <GrLocation className="icon"/>
                         </div>
                     </div>
+
+                    <div className="dateInput">
+                        <label htmlFor="date">Select your date:</label>
+                        <div className="input flex">
+                            <input type="date"/>
+                        </div>
+                    </div>
+
+                    <div className="priceInput">
+                        <div className="label_total flex">
+                            <label htmlFor="price">Max price:</label>
+                            <h3 className="total">$5000</h3>
+                        </div>
+                        <div className="input flex">
+                            <input type="range" max="5000" min="1000"/>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
 
